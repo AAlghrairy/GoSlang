c102864c82d8ec774d2efd6f2498efa431a52d2f
AAlghrairy <abdulelah.faisal@gmail.com>
2024-03-29 15:50:38 +0800

    Create ECE class, attempt to use go2json

---
+++ Go-Slang/ECE.ts
@@ -0,0 +1,10 @@
+import go2json = require("./go2json/go2json.js")
+
+export class ECE {
+    public static execute(sourceCode: string): any[] {
+        let C = go2json.go2ast(sourceCode);
+        return C;
+    }
+}
+
+export default ECE;
